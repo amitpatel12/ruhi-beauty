112e70f493c2bc389f2ec03bece987d1bd400d8c
amitpatel12 <patelamitkumar153@gmail.com>
2024-05-26 09:30:58 +0530

    mait

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-require("./db/config.js");
+require("./db/config");
 
 app.use("/", form);
 app.listen(PORT, () => console.log("Be started"));
